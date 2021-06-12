2348039777d52cbe3f965292ccd294f4b42d8daf
Matthew <mattj20@gmail.com>
2021-06-12 16:48:43 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collec)
+    i=0
+    new_collection = []
+    while i < collec.length
+        new_collection << yield(collec[i])
+        i+=1
+    end
+    new_collection
+end
+
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) {|lang| lang.capitalize}
 
 
